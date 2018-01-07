ef42d61b7cef0a8fb98318b688e1a270a9ebc878
JNDaniel <202167@edu.p.lodz.pl>
2018-01-07 15:12:45 +0100

    Main test

--- src/bazafilmow/Main.java
+++ src/bazafilmow/Main.java
@@ -7,9 +7,6 @@ package bazafilmow;
 
 import bazafilmow.model.*;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import org.postgresql.util.PSQLException;
 
 /**
  *
