555c1a0a0820e253c1245649143a858ff4e94f7c
JNDaniel <202167@edu.p.lodz.pl>
2018-01-07 01:12:31 +0100

    Poprawa metody equals

--- src/bazafilmow/model/Gatunek.java
+++ src/bazafilmow/model/Gatunek.java
@@ -107,15 +107,11 @@ public class Gatunek implements Serializable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof Gatunek)) {
             return false;
         }
         Gatunek other = (Gatunek) object;
-        if ((this.gatunekId == null && other.gatunekId != null) || (this.gatunekId != null && !this.gatunekId.equals(other.gatunekId))) {
-            return false;
-        }
-        return true;
+        return this.nazwa.equalsIgnoreCase(other.getNazwa());
     }
 
     @Override
